88a99e9d09eb05b1febec16903b1e08e1817699e
lopesboa <lopes.boa@sanar.com>
2023-02-21 00:28:56 -0300

    feat: create UserRepository implementation & add into infra DI

--- BuberDinner.Infrastructure/DependencyInjection.cs
+++ BuberDinner.Infrastructure/DependencyInjection.cs
@@ -6,17 +6,20 @@ using Microsoft.Extensions.DependencyInjection;
 using BuberDinner.Infrastructure.Authentication;
 using BuberDinner.Application.Common.Interfaces.Services;
 using BuberDinner.Application.Common.Interfaces.Authentication;
+using BuberDinner.Infrastructure.Persistence;
+using BuberDinner.Application.Common.Interfaces.Persistence;
 
 public static class DependencyInjection
 {
   public static IServiceCollection AddInfrastructure(
-    this IServiceCollection services, 
+    this IServiceCollection services,
     ConfigurationManager configuration
   )
   {
     services.Configure<JwtSettings>(configuration.GetSection(JwtSettings.SectionName));
     services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
     services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
+    services.AddSingleton<IUserRepository, UserRepository>();
 
     return services;
   }
