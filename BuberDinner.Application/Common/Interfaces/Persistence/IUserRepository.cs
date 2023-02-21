dc3b3c1e31b0fd5d65b0de899cd9b9a66722ebbf
lopesboa <lopes.boa@sanar.com>
2023-02-21 00:26:28 -0300

    feat: create user repository interface

---
+++ BuberDinner.Application/Common/Interfaces/Persistence/IUserRepository.cs
@@ -0,0 +1,8 @@
+using BuberDinner.Domain.Entities;
+namespace BuberDinner.Application.Common.Interfaces.Persistence;
+public interface IUserRepository
+{
+  User? GetUserByEmail(string email);
+  void Add(User user);
+
+}
